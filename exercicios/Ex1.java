13a0581862b7f9bff9e133585321a41dc2d44db8
eduardoluizbogo <53580431+eduardoluizbogo@users.noreply.github.com>
2019-08-19 11:07:34 -0300

    Add files via upload

---
+++ exercicios/Ex1.java
@@ -0,0 +1,39 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int tamanho=0;
+		int aux;
+		
+		System.out.println("Insira o tamanho do vetor: ");
+		tamanho = entrada.nextInt();
+		
+		int[] vet = new int[tamanho];
+		
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = entrada.nextInt();
+		}
+		for (int i = 0; i < (vet.length-1); i++) {
+			for (int j = i+1; j < vet.length; j++) {
+				if(vet[i]>vet[j]) {
+					aux = vet[i];
+					vet[i]=vet[j];
+					vet[j]=aux;
+					
+				}
+			}
+		}
+		for (int i = 0; i < vet.length; i++) {
+			System.out.println(vet[i]);
+		}
+		entrada.close();
+		}
+	
+}
+
