d36bca6dc858074552246d58c1a4b042e2c2d479
eduardoluizbogo <53580431+eduardoluizbogo@users.noreply.github.com>
2019-08-19 11:25:47 -0300

    Add files via upload

---
+++ exercicios/Ex3.java
@@ -0,0 +1,38 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int tamanho=0;
+		String aux;
+		
+		System.out.println("Insira o tamanho do vetor: ");
+		tamanho = entrada.nextInt();
+		
+		String[] vet = new String[tamanho];
+		
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = entrada.next();
+		}
+		for (int i = 0; i < (vet.length-1); i++) {
+			for (int j = i+1; j < vet.length; j++) {
+				if(vet[i].compareTo(vet[j]) > 0) {
+					aux = vet[i];
+					vet[i] = vet[j];
+					vet[j] = aux;
+					
+				}
+			}
+		}
+		for (int i = 0; i < vet.length; i++) {
+			System.out.println(vet[i]);
+		}
+		entrada.close();
+		}
+	
+}
